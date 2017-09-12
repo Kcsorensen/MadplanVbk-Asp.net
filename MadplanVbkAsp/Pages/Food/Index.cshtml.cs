daf54e3af0621bc55e338655d2c288fd2b69d0f0
Kim Sørensen <kcsorensen@outlook.dk>
2017-09-12 06:46:56 +0200

    v0.012

--- MadplanVbkAsp/Pages/Food/Index.cshtml.cs
+++ MadplanVbkAsp/Pages/Food/Index.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MadplanVbkAsp.Interfaces;
+using SharedLib.Models;
 
 namespace MadplanVbkAsp.Pages.Food
 {
     public class IndexModel : PageModel
     {
+        public List<Recipe> Recipes { get; set; }
+
+        public IndexModel(IRecipeData recipeData)
+        {
+            Recipes = recipeData.GetAll();
+        }
+
         public void OnGet()
         {
+
         }
     }
 }
